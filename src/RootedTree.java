24acbfa2faef362bf39fd234a49ecbeee158235b
mikael epelbaum <mikaelepelbaum@gmail.com>
2021-12-20 19:52:52 +0200

    before any code

---
+++ src/RootedTree.java
@@ -0,0 +1,11 @@
+import java.io.DataOutputStream;
+
+class RootedTree {
+
+    public RootedTree(){}
+
+    public void printByLayer(DataOutputStream out) {}
+
+    public void preorderPrint(DataOutputStream out) {}
+
+}
